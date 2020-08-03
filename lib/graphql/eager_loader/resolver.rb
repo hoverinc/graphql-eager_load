82fa808f15b8c7e9d19c4038e3f78ba579970375
Elise Wood <elise@hover.to>
2020-08-03 12:31:21 -0700

    init

--- lib/graphql/eager_loader/resolver.rb
+++ lib/graphql/eager_loader/resolver.rb
@@ -9,7 +9,7 @@ module Graphql
       end
 
       def associations_to_include
-        graphql_eager_load_options(model: @@eager_load_model)
+        graphql_eager_load_options(model: self.class.class_variable_get(:@@eager_load_model))
       end
 
       def graphql_eager_load_options(selections: context.query.lookahead.selections, model:)
@@ -20,7 +20,9 @@ module Graphql
       # in the graphql query for this resolver.
       module ClassMethods
         def eager_load_model(model)
-          @eager_load_model = model
+          # rubocop:disable Style/ClassVars
+          class_variable_set(:@@eager_load_model, model)
+          # rubocop:enable Style/ClassVars
         end
       end
     end
